9c9dd3e29d1711cff2a601a5ead906839190f5da
raul ramirez <ramirez.rulo@gmail.com>
2018-06-19 17:53:07 -0400

    add query function.

--- scripts/index.js
+++ scripts/index.js
@@ -8,20 +8,60 @@
 
 
  
+    // var mathForBooks = 0;
 
-    function numberOfBooks (){
-        var a = document.querySelector('btn.quantity').value; // it is not taking the class/value. Can't use "btn btn-default" as a class, shows an error.
-        console.log(a);
+    function numberOfBooks (value){
+       var a = document.querySelector('.btn').value; // it is not taking the class/value. Can't use "btn btn-default" as a class, shows an error.
+        console.log(value);
         //this.quanityBooks =
+        // mathForBooks=value;
+        // console.log('math number is ' + mathForBooks);
+        // getPrice();
     };
 
-    var quanityQuantity = document.querySelector(Option.Quantity);
-    var quantityOne = document.querySelector(Option.One);
-    var quantityTwo = document.querySelector(Option.Two);
-    var quantityThree = document.querySelector(Option.Three);
-    var quantityFour = document.querySelector(Option.Four);
-    var quantityFive = document.querySelector(Option.Five);
+    // function getPrice (){
+    //     var finalPrice = 0;
+    //     finalPrice = 17*mathForBooks;
+    //     console.log(finalPrice);
+    //     document.querySelector(.fin).innerHTML=finalPrice; 
+    //     return finalPrice;
+    // };
 
-    quanityQuantity.addEventListener('click', (i) =>{
+numberOfBooks();
 
-    }
+
+    var quanityQuantity = document.querySelector('select');
+    //var quantityOne = document.querySelector(Option.opOne);
+    // var quantityTwo = document.querySelector(Option.Two);
+    // var quantityThree = document.querySelector(Option.Three);
+    // var quantityFour = document.querySelector(Option.Four);
+    // var quantityFive = document.querySelector(Option.Five);
+    //console.log(quantityOne);
+
+    document.querySelector('select').addEventListener('change', numberOfBooks(this.value));
+
+
+    // quantityOne.addEventListener('click', (i) =>{
+    // numberOfBooks = 1;
+    // console.log(numberOfBooks);
+    // });
+
+    // quantityTwo.addEventListener('click', (i) =>{
+    // numberOfBooks = 2;
+    // console.log(numberOfBooks);
+    // });
+
+    //  quantityThree.addEventListener('click', (i) =>{
+    // numberOfBooks = 3;
+    // console.log(numberOfBooks);
+    // });
+
+    // quantityFour.addEventListener('click', (i) =>{
+    // numberOfBooks = 4;
+    // console.log(numberOfBooks);
+    // });
+
+    // quantityFour.addEventListener('click', (i) =>{
+    // numberOfBooks = 5;
+    // console.log(numberOfBooks);
+    // });
