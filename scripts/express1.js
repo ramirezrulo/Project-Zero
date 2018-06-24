eb207141d86b76cde394667a848ea6d2c1981e87
raul ramirez <ramirez.rulo@gmail.com>
2018-06-24 14:16:04 -0400

    add router.

--- scripts/express1.js
+++ scripts/express1.js
@@ -19,13 +19,14 @@ var path = require('path');
 app.use(express.static('img'));
 app.use(express.static('styles'));
 app.use(express.static('scripts'));
+app.use(express.static('styles'));
 
 app.get('/',(request,response) => {
  response.sendFile(path.join(__dirname,'../index.html'));
 });
 
 app.get('/contact',(request,response) => {
- response.sendFile(path.join(__dirname,'../contact.html'));
+ response.sendFile(path.join(__dirname,'../contact-us.html'));
 });
 
 app.listen(5050);
