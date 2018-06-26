895ca15b6b6e91aba571945cd4f5295ea21c9cb7
raul ramirez <ramirez.rulo@gmail.com>
2018-06-26 08:40:16 -0400

    add javascript working on the server.

--- scripts/express1.js
+++ scripts/express1.js
@@ -22,137 +22,144 @@ app.use(express.static('styles'));
 app.use(express.static('scripts'));
 
 app.route("/")
-.get((req, res) => {
-res.render('index');
-});
+    .get((req, res) => {
+        res.render('index');
+    });
 
 app.route("/about-us")
-.get((req, res) => {
-res.render('about-us');
-});
+    .get((req, res) => {
+        res.render('about-us');
+    });
 
 app.route("/contact-us")
-.get((req, res) => {
-res.render('contact-us');
-});
+    .get((req, res) => {
+        res.render('contact-us');
+    });
 
 app.route("/sales")
-.get((req, res) => {
-res.render('sales');
-});
+    .get((req, res) => {
+        res.render('sales');
+    });
 
 app.route("/popular")
-.get((req, res) => {
-res.render('popular');
-});
+    .get((req, res) => {
+        res.render('popular');
+    });
 
 app.route("/kids")
-.get((req, res) => {
-res.render('kids');
-});
+    .get((req, res) => {
+        res.render('kids');
+    });
 
 app.route("/teens")
-.get((req, res) => {
-res.render('teens');
-});
+    .get((req, res) => {
+        res.render('teens');
+    });
 
 app.route("/electronic-books")
-.get((req, res) => {
-res.render('electronic-books');
-});
+    .get((req, res) => {
+        res.render('electronic-books');
+    });
 
 app.route("/audiobooks")
-.get((req, res) => {
-res.render('audiobooks');
-});
+    .get((req, res) => {
+        res.render('audiobooks');
+    });
 
 app.route("/accessories")
-.get((req, res) => {
-res.render('accessories');
-});
+    .get((req, res) => {
+        res.render('accessories');
+    });
 
 app.route("/bookstore-recommendation")
-.get((req, res) => {
-res.render('bookstore-recommendation');
-});
+    .get((req, res) => {
+        res.render('bookstore-recommendation');
+    });
 
 app.route("/new-releases")
-.get((req, res) => {
-res.render('new-releases');
-});
+    .get((req, res) => {
+        res.render('new-releases');
+    });
 
 app.route("/most-popular")
-.get((req, res) => {
-res.render('most-popular');
-});
+    .get((req, res) => {
+        res.render('most-popular');
+    });
 
 app.route("/best-sellers")
-.get((req, res) => {
-res.render('best-sellers');
-});
+    .get((req, res) => {
+        res.render('best-sellers');
+    });
 
 ////////////////BOOKS/////////////////////
 
 app.route("/the-outsider")
-.get((req, res) => {
-res.render('the-outsider');
-});
+    .get((req, res) => {
+        res.render('the-outsider');
+    });
 
 app.route("/the-alchemist")
-.get((req, res) => {
-res.render('the-alchemist');
-});
+    .get((req, res) => {
+        res.render('the-alchemist');
+    });
 
 app.route('/the-12-rules-for-life')
-.get((req, res) => {
-res.render('the-12-rules-for-life');
-});
+    .get((req, res) => {
+        res.render('the-12-rules-for-life');
+    });
 
 app.route("/you-are-a-badass")
-.get((req, res) => {
-res.render('you-are-a-badass');
-});
+    .get((req, res) => {
+        res.render('you-are-a-badass');
+    });
 
 app.route("/elevation")
-.get((req, res) => {
-res.render('elevation');
-});
+    .get((req, res) => {
+        res.render('elevation');
+    });
 
 app.route("/the-4-hour-work-week")
-.get((req, res) => {
-res.render('the-4-hour-work-week');
-});
+    .get((req, res) => {
+        res.render('the-4-hour-work-week');
+    });
 
 app.route("/robin")
-.get((req, res) => {
-res.render('robin');
-});
+    .get((req, res) => {
+        res.render('robin');
+    });
 
 app.route("/milk-and-honey")
-.get((req, res) => {
-res.render('milk-and-honey');
-});
+    .get((req, res) => {
+        res.render('milk-and-honey');
+    });
 
 app.route("/think-and-grow-rich")
-.get((req, res) => {
-res.render('think-and-grow-rich');
-});
+    .get((req, res) => {
+        res.render('think-and-grow-rich');
+    });
 
 app.route("/kings-cage")
-.get((req, res) => {
-res.render('kings-cage');
-});
+    .get((req, res) => {
+        res.render('kings-cage');
+    });
 ////////////////END OF BOOKS//////////////
 
 app.route("/instant-purchase")
-.get((req, res) => {
-res.render('instant-purchase');
-});
+    .get((req, res) => {
+        res.render('instant-purchase');
+    });
 
 app.route("/add-to-cart-the-outsider")
-.get((req, res) => {
-res.render('add-to-cart-the-outsider');
-});
-
+    .get((req, res) => {
+        res.render('add-to-cart-the-outsider');
+    })
+    .get((req, res) => {
+        res.write('index.js');
+    });
+    // .get((req, res) => {
+       
+    //     res.render('index');
+    // });
+   
 
 app.listen(5050);
--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,25 @@
-function numberOfBooks (numBooks){
+function numberOfBooks(numBooks) {
     console.log("in numberofbooks: " + numBooks);
 
     var title = document.querySelector('#bookTitle').textContent;
-    
+
     var subtotal = document.querySelector('#subtotalPrice').textContent;
 
     console.log("SubTotal: " + subtotal);
-    
-    finalPrice = numBooks * Number( subtotal );
+
+    finalPrice = numBooks * Number(subtotal);
     console.log("finalPrice: " + finalPrice);
 
     sessionStorage.setItem(title, '' + numBooks + "," + finalPrice);
 
     document.querySelector('#totalPrice').textContent = finalPrice;
     return numBooks;
-  };
+};
 
-    var books = document.querySelector('select');
 
-  books.addEventListener('change', (e) => {
-      numberOfBooks(e.target.value);
-  });
+var books = document.querySelector('select');
 
-  
+books.addEventListener('change', (e) => {
+    numberOfBooks(e.target.value);
+});
+
